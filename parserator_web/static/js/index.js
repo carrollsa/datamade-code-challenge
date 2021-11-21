e75decd21422ec615d03dd6f4c27439bf139c35f
Stephen Carroll <stephenalexandercarroll@gmail.com>
2021-11-21 18:57:10 -0500

    Parserator functioning

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,75 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+'use strict'
+
+function parseSuccess(data) {
+    
+    // Hide error and display results on successful api call
+    document.getElementById('error-results').style = "display: none"
+    document.getElementById('address-results').style = "display: block"
+
+    // Select elements to update
+    var existingTable = document.querySelector('table.table tbody')
+    var addressTypeSpan = document.getElementById('parse-type')
+
+    // Render address type
+    addressTypeSpan.innerText = data.address_type
+
+    // Generate and append address components to existing table 
+    let extendedTable = generateExtendedTable(data.address_components)
+    existingTable.parentNode.replaceChild(extendedTable, existingTable)
+}
+
+function generateExtendedTable(data) {
+    let extendedTable = document.createElement('tbody')
+    for (const [key, value] of Object.entries(data)) {
+        let newRow = generateTableRow(key, value)
+        extendedTable.appendChild(newRow)
+    }
+
+    return extendedTable
+}
+
+function generateTableRow(key, value) {
+    let newRow = document.createElement('tr')
+    let keyCell = document.createElement('td')
+    let valueCell = document.createElement('td')
+
+    keyCell.appendChild(document.createTextNode(key))
+    valueCell.append(document.createTextNode(value))
+    newRow.appendChild(keyCell)
+    newRow.appendChild(valueCell)
+
+    return newRow
+}
+
+function parseFailure(errorResponse) {
+    // Hide previous results
+    document.getElementById('address-results').style = "display: none"
+    
+    var errorDiv = document.getElementById('error-results')
+
+    errorDiv.style = "display: block"
+    errorDiv.innerHTML = errorResponse
+}
+
+document.getElementById('submit').addEventListener('click', function(event) {
+
+    event.preventDefault()
+
+    const api = ('/api/parse?')
+    const inputString = document.getElementById('address').value
+
+    if(inputString) {
+        return fetch(api + new URLSearchParams({
+            address: inputString
+        }))
+            .then(async (res) => {
+                if (res.ok) {
+                    return await res.json()
+                } else {
+                    throw ((await res.json()).detail)
+                }
+            }) 
+            .then((data) => parseSuccess(data))
+            .catch((e) => parseFailure(e))
+    }
+})
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,9 +16,24 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        
+        input_string = request.query_params['address']
+        
+        address_components, address_type = self.parse(input_string)
+        return Response({
+            'input_string': input_string,
+            'address_components': address_components,
+            'address_type': address_type
+        })
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        
+        try:
+            address_components, address_type = usaddress.tag(address)
+        except usaddress.RepeatedLabelError as e:
+            # Copied from DataMade's Parserator
+            raise ParseError(detail='Unable to parse this value due to repeated labels. Our team has been notified of the error.')
+        
         return address_components, address_type
